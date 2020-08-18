29c4193d38c989592864c027ca145acb3c532274
sonic247897 <sonic247897@gmail.com>
2020-08-18 23:16:04 +0900

    bitmask

---
+++ BOJ/Algorithm/BitMask_11723.java
@@ -0,0 +1,58 @@
+package Algorithm;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+// @ 바꾸지 말고 해보자. 2배로 수가 증가하는 거 말고 연산이 바뀌는게 있나?
+//	-> 이 문제는 bitmask수를 배열의 index로 쓰지 않기 때문에 상관 없지만, 
+//	   만약에 배열의 인덱스로 쓰게 된다면 배열의 크기가 2배 증가하게 된다.
+// {1,2,..,20} => {0,1,..,19}로 변경 (전처리 안하면 최대 정수의 크기가 2배가 됨)
+// 2^0+2^1+...+2^19 = 2^20-1 
+//	-> int형 정수로 나타낼 수 있으므로 bitmask 이용
+public class BitMask_11723 {
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int S = 0; // 공집합
+		
+		try {
+			int m = Integer.parseInt(br.readLine());
+			String[] tmp;
+			StringBuilder sb = new StringBuilder();
+			for(int i=0; i<m; ++i) {
+				tmp = br.readLine().split(" ");
+				String cmd = tmp[0];
+				switch(cmd) {
+					case "add":
+						S |= (1 << Integer.parseInt(tmp[1]));
+						break;
+					case "remove":
+						S &= ~(1 << Integer.parseInt(tmp[1]));
+						break;
+					case "check": // 집합 S의 값은 변하면 안된다.
+						int check = S & (1 << Integer.parseInt(tmp[1]));
+						if(check > 0) sb.append('1').append('\n');
+						else sb.append('0').append('\n');
+						break;
+					case "toggle":
+						S ^= (1 << Integer.parseInt(tmp[1]));
+						break;
+					case "all":
+						S = (1<<21)-1; // 2^0+2^1+..+2^20 = 2^21-1
+						break;
+					default: // empty
+						S = 0;
+				}
+			}
+			bw.write(sb.toString());
+			bw.flush();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
--- BOJ/Algorithm/BruteForce_1248.java
+++ BOJ/Algorithm/BruteForce_1248.java
@@ -23,14 +23,20 @@ public class BruteForce_1248 {
 
 			go(0, n, a, sum);
 			
+			for(int i=0; i<n; ++i) {
+				bw.write(Integer.toString(a[i]));
+				bw.write(' ');
+			}
+			bw.flush();
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	static boolean check(int idx, int[] a, char[][] sum) {
+		// sum[i][idx]: i범위 = 0 ~ idx-1
 		for(int i=0; i<idx; ++i) {
-			// i < idx
 			int tmp = 0;
 			// sum[i][j] = a[i]~a[j]까지의 합
 			for(int j=i; j<=idx; ++j) {
@@ -38,13 +44,13 @@ public class BruteForce_1248 {
 			}
 			// 한 번이라도 결과 잘 못 나오면 false
 			if(tmp > 0) {
-				if(sum[idx][i] != '+')
+				if(sum[i][idx] != '+')
 					return false;
 			}else if(tmp < 0){
-				if(sum[idx][i] != '-')
+				if(sum[i][idx] != '-')
 					return false;
 			}else {
-				if(sum[idx][i] != '0')
+				if(sum[i][idx] != '0')
 					return false;
 			}
 		}
@@ -52,27 +58,28 @@ public class BruteForce_1248 {
 	}
 	
 	// 배열 a[] 채워서 리턴 - a의 idx번째 수 정하고 다음경우 호출
-	static void go(int idx, int n, int[] a, char[][] sum) {
+	static boolean go(int idx, int n, int[] a, char[][] sum) {
 		// 모든 수 정하면 검사하고 리턴 - 앞에서 먼저 검사하므로 그냥 리턴
-		if(idx == n) {
-			
-			return;
-		}
-	
+		if(idx == n) return true;		
 		// 다음 경우 호출 - idx열 검사(백트래킹)
+		boolean valid = false; // @ 재귀 함수의 리턴값에 들어갈 값을 저장할 변수를 선언해야 한다.
+		// '0'이라면 0을 넣는 선택지 밖에 없으므로 따로 처리
 		if(sum[idx][idx] == '0') {
 			a[idx] = 0;
-			if(check(idx, a, sum)) {
-				go();
-				return;
-			}
-			// 결과가 false면 앞에서부터 잘못 
+			if(check(idx, a, sum)) // @@ 백트래킹
+				return go(idx+1, n, a, sum); // 결과가 false면 앞에서부터 잘못 만든 것이므로 true 나올 때까지 리턴
 		}
-			
+		
+		int sign = 1;
+		if(sum[idx][idx] == '-') sign = -1;
 		
 		for(int i=1; i<=10; ++i) {
-			
+			a[idx] = sign*i;
+			if(check(idx, a, sum)) // @@ 백트래킹
+				valid = go(idx+1, n, a, sum);
+			if(valid == true) return true;
 		}
-		
+		// valid == true일 때만 계속 리턴되므로 재귀가 끝나면 올바른 a가 나온다.
+		return false;
 	}
 }
